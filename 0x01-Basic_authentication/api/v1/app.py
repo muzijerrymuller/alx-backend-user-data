2331a5e9bcfe3d3a4736fc1bdd75fb0a7219a595
muzijerrymuller <jerrymuller883@gmail.com>
2024-11-14 16:55:06 +0000

    index

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 """
-Route module for the API
+API Routing module
 """
 from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
-
+from typing import Literal
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -16,9 +16,18 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 @app.errorhandler(404)
 def not_found(error) -> str:
-    """ Not found handler
+    """
+    404 error handler
     """
     return jsonify({"error": "Not found"}), 404
+
+
+@app.errorhandler(401)
+def unauthorized(error) -> tuple[str, Literal[401]]:
+    """
+    404 error handler
+    """
+    return jsonify({"error": "unauthorized"}), 401
 
 
 if __name__ == "__main__":
--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -25,3 +25,9 @@ def stats() -> str:
     stats['users'] = User.count()
     return jsonify(stats)
 
+@app_views.route('/unauthorized/', methods=['GET'], strict_slashes=False)
+def unauthorized() -> str:
+    '''
+    Handles unauthorized access 401
+    '''
+    return abort(401)
