f6e33d7d8f7f1270cd90f418250a15772f538887
muzijerrymuller <jerrymuller883@gmail.com>
2024-11-14 17:31:04 +0000

    index pycode style error fix

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -27,6 +27,7 @@ def stats() -> str:
     stats['users'] = User.count()
     return jsonify(stats)
 
+
 @app_views.route('/unauthorized/', methods=['GET'], strict_slashes=False)
 def unauthorized() -> str:
     """
